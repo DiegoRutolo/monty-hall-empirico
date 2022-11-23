422dc1cb825d6ae9ffa9e5c351a2af2bf0d431c7
DiegoRutolo <diegorutolo@gmail.com>
2022-11-23 10:38:25 +0100

    Añadidas iteraciones. Bug que a veces asigna varios coches

--- montyHall.py
+++ montyHall.py
@@ -6,12 +6,15 @@ class Puerta():
         self.elegida = False
     
     def __str__(self):
-        return "<{}>".format("COCHE" if self.contenido else "CABRA")
+        #return "<{}>".format("COCHE" if self.contenido else "CABRA")
+        return "<{}>".format(self.contenido)
 
 class Concurso:
     puertas = []
 
-    def __init__(self):
+    def __init__(self, log=False):
+        self.log = log
+
         self.puertas.append(Puerta(False))
         self.puertas.append(Puerta(False))
         self.puertas.append(Puerta(True))
@@ -19,7 +22,8 @@ class Concurso:
 
         self.eleccion = None
 
-        print("Se crean las puertas {}".format(self.getTexto()))
+        if self.log:
+            print("Se crean las puertas {}".format(self.getTexto()))
     
     def getTexto(self):
         txt = "[{}, {}, {}]".format(self.puertas[0], self.puertas[1], self.puertas[2])
@@ -30,35 +34,66 @@ class Concurso:
             if num == i:
                 self.eleccion = self.puertas[i]
                 self.puertas[i].elegida = True
-                print("Elige la puerta {}".format(i))
+                if self.log:
+                    print("Elige la puerta {}".format(i))
             else:
                 self.puertas[i].elegida = False
     
     def revelar(self):
         for i in range(len(self.puertas)):
             if (not self.puertas[i].elegida) and (not self.puertas[i].contenido):
-                print("Se revela la puerta {}".format(i))
+                if self.log:
+                    print("Se revela la puerta {}".format(i))
                 return i
     
     def conclusion(self):
         return self.eleccion.contenido
 
-def jugar(cambiar):
-    concurso = Concurso()
+def jugar(cambiar, log=False):
+    concurso = Concurso(log=log)
     
     opt1 = random.randint(0, 2)
     concurso.elegir(opt1)
     revelada = concurso.revelar()
     
     if cambiar:
-        print("Decide cambiar de puerta")
+        if log:
+            print("Decide cambiar de puerta")
         puertas = [0, 1, 2]
         puertas.remove(revelada)
         puertas.remove(opt1)
         concurso.elegir(puertas[0])
     
     gana = concurso.conclusion()
-    print("El resultado final es {}".format("COCHE" if gana else "CABRA"))
+    if log:
+        print("El resultado final es {}".format("COCHE" if gana else "CABRA"))
+    return gana
+
+
 
 if __name__ == "__main__":
-    jugar(True)
+    numIteraciones = 8
+
+    # Manteniendo
+    cuenta1 = {
+        True: 0,
+        False: 0
+    }
+
+    for i in range(numIteraciones):
+        result = jugar(False)
+        cuenta1[result] += 1
+    
+    print(cuenta1)
+
+    # Cambiando
+    cuenta2 = {
+        True: 0,
+        False: 0
+    }
+
+    for i in range(numIteraciones):
+        result = jugar(True, log=True)
+        cuenta2[result] += 1
+
+    print(cuenta2)
